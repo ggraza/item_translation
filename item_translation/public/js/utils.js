50ef32edac1cfd4ce1fca94a27c5cc599051438e
barredterra <14891507+barredterra@users.noreply.github.com>
2026-04-04 16:00:29 +0200

    refactor: extract shared translation fetch and apply helpers in utils.js

--- item_translation/public/js/utils.js
+++ item_translation/public/js/utils.js
@@ -1,26 +1,34 @@
 frappe.provide("item_translation"); //create namespace
 
 item_translation.utils = {
+	_fetch_translation: function (item_code, lang) {
+		return frappe.db.get_list("Item Translation", {
+			fields: ["name", "language", "item_name", "description"],
+			filters: { language: lang, item: item_code },
+			limit: 1,
+		});
+	},
+
+	_apply_translation: function (cdt, cdn, translation) {
+		if (translation.item_name) {
+			frappe.model.set_value(cdt, cdn, "item_name", translation.item_name);
+		}
+		if (translation.description) {
+			frappe.model.set_value(cdt, cdn, "description", translation.description);
+		}
+	},
+
 	get_translated_description: async function (frm, cdt, cdn) {
 		const row = locals[cdt][cdn];
 		const lang = frm.doc.language;
 		const settings = await frappe.db.get_doc("Item Translation Settings");
 
 		if (settings.language != lang) {
-			const res = await frappe.db.get_list("Item Translation", {
-				fields: ["name", "language", "item_name", "description"],
-				filters: { language: lang, item: row.item_code },
-				limit: 1,
-			});
+			const res = await this._fetch_translation(row.item_code, lang);
 			setTimeout(() => {
 				if (res.length > 0) {
-					if (res[0].item_name) {
-						frappe.model.set_value(cdt, cdn, "item_name", res[0].item_name);
-					}
-					if (res[0].description) {
-						frappe.model.set_value(cdt, cdn, "description", res[0].description);
-					}
-				} else if (settings.display_warnings === 1) {
+					this._apply_translation(cdt, cdn, res[0]);
+				} else if (settings.display_warnings == 1) {
 					frappe.msgprint(
 						__(
 							"Translation missing for <b> {0} </b> in language <b> {1}</b>. Please change print language in customer or add <b>Item Translation</b>",
@@ -31,17 +39,11 @@ item_translation.utils = {
 			}, 500);
 		}
 	},
+
 	refetch_all_translations: async function (frm) {
-		// check if there are any items in item table
-		if (frm.doc.items.length == 0) {
-			return;
-		} else if (frm.doc.items.length == 1) {
-			if (!frm.doc.items[0].item_code) {
-				return;
-			} else if (frm.doc.items[0].item_code == "") {
-				return;
-			}
-		}
+		if (frm.doc.items.length == 0) return;
+		if (frm.doc.items.length == 1 && !frm.doc.items[0].item_code) return;
+
 		const lang = frm.doc.language;
 		const settings = await frappe.db.get_doc("Item Translation Settings");
 		frappe.confirm(
@@ -50,32 +52,11 @@ item_translation.utils = {
 				[lang]
 			),
 			async () => {
-				// If yes loop through all items and refetch Item Description
-				for (row of frm.doc.items) {
-					// check if description needs to come from normal item or translation
+				for (const row of frm.doc.items) {
 					if (settings.language != lang) {
-						let res = await frappe.db.get_list("Item Translation", {
-							fields: ["name", "language", "item_name", "description"],
-							filters: { language: lang, item: row.item_code },
-							limit: 1,
-						});
+						const res = await this._fetch_translation(row.item_code, lang);
 						if (res.length > 0) {
-							if (res[0].item_name) {
-								frappe.model.set_value(
-									row.doctype,
-									row.name,
-									"item_name",
-									res[0].item_name
-								);
-							}
-							if (res[0].description) {
-								frappe.model.set_value(
-									row.doctype,
-									row.name,
-									"description",
-									res[0].description
-								);
-							}
+							this._apply_translation(row.doctype, row.name, res[0]);
 						} else if (settings.display_warnings == 1) {
 							frappe.msgprint(
 								__(
@@ -85,32 +66,19 @@ item_translation.utils = {
 							);
 						}
 					} else {
-						let res = await frappe.db.get_list("Item", {
+						const res = await frappe.db.get_list("Item", {
 							fields: ["name", "item_name", "description"],
 							filters: { item_code: row.item_code },
 							limit: 1,
 						});
-
 						if (res.length > 0) {
-							frappe.model.set_value(
-								row.doctype,
-								row.name,
-								"item_name",
-								res[0].item_name
-							);
-							frappe.model.set_value(
-								row.doctype,
-								row.name,
-								"description",
-								res[0].description
-							);
+							frappe.model.set_value(row.doctype, row.name, "item_name", res[0].item_name);
+							frappe.model.set_value(row.doctype, row.name, "description", res[0].description);
 						}
 					}
 				}
 			},
-			() => {
-				// action to perform if No is selected
-			}
+			() => {}
 		);
 	},
 };
